9b2fdfcc6b84fbdb45544a5de2839235f3d670f3
hahahxh <huangxinheng@gmail.com>
2017-03-23 22:35:16 -0700

    102

---
+++ levelOrder.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> res;
+        if(root==NULL)
+            return res;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            int size=q.size();
+            vector<int> sub;
+            for(int i=0;i<size;i++){
+                TreeNode* t=q.front();
+                q.pop();
+                sub.push_back(t->val);
+                if(t->left)
+                    q.push(t->left);
+                if(t->right)
+                    q.push(t->right);
+            }
+            res.push_back(sub);
+        }
+        return res;
+    }
+};
