5a9112782abe27b8d4d9304cee7492df7652c965
hahahxh <huangxinheng@gmail.com>
2017-03-26 17:54:33 -0700

    122

---
+++ maxProfit.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if(prices.empty())
+            return 0;
+        int maxProfit=0;
+        for(int i=0;i<prices.size();i++){
+            if(i>0 && prices[i]>prices[i-1])
+                maxProfit+=prices[i]-prices[i-1];
+        }
+        return maxProfit;
+    }
+};
