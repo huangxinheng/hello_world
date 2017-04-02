2860697eb86d6d21afbc0bcb9c19eca029329626
hahahxh <huangxinheng@gmail.com>
2017-04-01 17:11:03 -0700

    56

---
+++ merge.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for an interval.
+ * struct Interval {
+ *     int start;
+ *     int end;
+ *     Interval() : start(0), end(0) {}
+ *     Interval(int s, int e) : start(s), end(e) {}
+ * };
+ */
+class Solution {
+public:
+    static bool cmp(Interval a,Interval b){
+        return a.start < b.start;
+    }
+    vector<Interval> merge(vector<Interval>& intervals) {
+        if(intervals.size()<=1)
+            return intervals;
+        sort(intervals.begin(),intervals.end(),cmp);
+        vector<Interval> res;
+        res.push_back(intervals[0]);
+        for(int i=1;i<intervals.size();i++){
+            if(res.back().end >=intervals[i].start){
+                res.back().end=max(res.back().end,intervals[i].end);
+            }else{
+                res.push_back(intervals[i]);
+            }
+        }
+        return res;
+    }
+};
