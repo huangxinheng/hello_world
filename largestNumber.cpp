b4fd4751d2af35278dd6acf01026ccfc524fac61
hahahxh <huangxinheng@gmail.com>
2017-04-04 23:44:28 -0700

    179

--- largestNumber.cpp
+++ largestNumber.cpp
@@ -1,2 +1,15 @@
-:x
-
+class Solution {
+public:
+    static bool cmp(int& a,int& b){
+        return (to_string(a)+to_string(b))>(to_string(b)+to_string(a));
+    }
+    string largestNumber(vector<int>& nums) {
+        string res="";
+        sort(nums.begin(),nums.end(),cmp);
+        for(auto n:nums)
+            res+=to_string(n);
+        if(res[0]=='0')
+            return "0";
+        return res;
+    }
+};
